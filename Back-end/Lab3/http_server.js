e36b6f7112a0ff74e948725ba5649106b52a2bbc
Aditi Tiwari <aditee0710@gmail.com>
2026-09-11 14:44:41 +0530

    FSD LAB

--- Back-end/Lab3/http_server.js
+++ Back-end/Lab3/http_server.js
@@ -1,34 +1,18 @@
-// import http from "http";
-
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
-//     const method = req.method;
-
-//     if (url == "/msg" && method == "GET") {
-//         res.statusCode = 200;
-//         res.setHeader("Content-Type", "text/plain");
-//         res.end("Welcome to backend");
-//     }
-// });
-
-// server.listen(3000, () => {
-//     console.log("Server is running on port 3000");
-// });
 import http from "http";
 const array = [
     {
         id: 1,
-        name: "Aditi",
+        name: "A",
         age: 20
     },
     {
         id: 2,
-        name: "Aaradhya",
+        name: "Ak",
         age: 21
     },
     {
         id: 3,
-        name: "Aalia",
+        name: "o",
         age: 22
     }
 ];
@@ -50,6 +34,26 @@ const app = http.createServer((req, res) => {
 
         res.end(JSON.stringify(array));
     }
+else if (url === "/user" && method === "POST") {
+
+    let body = "";
+
+    req.on("data", (content ) => {
+        body += content;
+    });
+
+    req.on("end", () => {
+
+        const user = JSON.parse(body);
+
+        array.push(user);
+
+        res.statusCode = 201;
+        res.setHeader("Content-Type", "application/json");
+
+        res.end(JSON.stringify(user));
+    });
+}
     else {
         res.statusCode = 404;
         res.setHeader("Content-Type", "text/plain");
@@ -57,6 +61,7 @@ const app = http.createServer((req, res) => {
         res.end("Route not found");
     }
 });
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
